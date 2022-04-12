6998da61d58c1e9a5bf83c7a27a6a964d15c7d95
colehearld <hearldcole@gmail.com>
2022-04-12 16:41:44 -0400

    ...

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,5 +1,13 @@
 const Migrations = artifacts.require("Migrations");
+const DaiTokenMock= artifacts.require("DaiTokenMock");
 
-module.exports = function(deployer) {
-  deployer.deploy(Migrations);
+module.exports = async function(deployer) {
+    await deployer.deploy(Migrations);
+    await artifacts.require(DaiTokenMock);
+    const tokenMock = await DaiTokenMock.deployed()
+    // Mint 1000 Dai Tokens for the deployer
+    await tokenMock.mint(
+        '0x2C6f53712777438105B88Aa3cA8bb8c405F72f66'
+        '1000000000000000000000'
+    )
 };
